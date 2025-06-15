53147dd7d076fd87d0e53212685773229231fd19
Jakub Małdrzykowski <kubamaldrzyk@gmail.com>
2025-06-15 14:56:03 +0200

    Forgot to commit notebook in last commit.

--- proj/state_of_art.py
+++ proj/state_of_art.py
@@ -5,7 +5,7 @@ State of the art method - spam classification with Multinomial Naive Bayes class
 '''
 from sklearn.naive_bayes import MultinomialNB
 from main import *
-import os 
+import os
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 import pandas as pd
@@ -13,9 +13,43 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split, cross_val_score
 
-def run_state_of_art(tokenization, feature_repr, tid):
-    csv_path = "data/enron_dataset.csv"
 
+def run_state_of_art(tokenization, feature_repr, tid):
+
+    clf = MultinomialNB()
+    fold_metrics = []
+
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    for train_index, test_index in skf.split(X_vect, y):
+        X_train, X_test = X_vect[train_index], X_vect[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+
+        metrics = evaluate(y_pred, y_test)
+        metrics['predictions'] = preds
+        metrics['true_labels'] = y
+
+        fold_metrics.append(metrics)
+        save_path = os.path.join("test_results_state_of_art", f"{tid}.npy")
+        np.save(save_path, metrics)
+
+    # accuracies = [m['accuracy'] for m in fold_metrics]
+    # print(f"Cross-validated accuracy: {np.mean(accuracies):.4f}")
+
+
+if __name__ == "__main__":
+
+    token_test_cases = {
+        "TK1": ("lemmatization+stopword", "tfidf"),
+        "TK2": ("lemmatization+stopword", "tf"),
+        "TK3": ("whitespace", "tfidf"),
+        "TK4": ("whitespace", "tf"),
+    }
+
+    csv_path = "data/enron_dataset.csv"
     df = load_enron_csv_dataset(csv_path)
 
     tokenizer = None
@@ -25,40 +59,10 @@ def run_state_of_art(tokenization, feature_repr, tid):
         tokenizer = simple_tokenizer
     else:
         raise ValueError(f"Unknown tokenization: {tokenization}")
-    
-    X_vect, vectorizer= extract_features(df['text'], tokenizer, vectorizer_type=feature_repr)
+
+    X_vect, vectorizer = extract_features(df['text'], tokenizer, vectorizer_type=feature_repr)
     y = df['label']
 
-    clf = MultinomialNB()
-    fold_metrics = []
-
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-    
-    for train_index, test_index in skf.split(X_vect, y):
-        X_train, X_test = X_vect[train_index], X_vect[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-
-        metrics = evaluate(y_pred, y_test)
-        fold_metrics.append(metrics)
-        save_path = os.path.join("test_results_state_of_art", f"{tid}.npy")
-        np.save(save_path, metrics)
-
-    # accuracies = [m['accuracy'] for m in fold_metrics]
-    # print(f"Cross-validated accuracy: {np.mean(accuracies):.4f}")
-
-if __name__ == "__main__":
-    test_cases = {
-    "T1": ("lemmatization+stopword", "tfidf"),
-    "T2": ("lemmatization+stopword", "tf"),
-    "T3": ("whitespace", "tf"),
-    "T4": ("whitespace", "tf"),
-    }
     for tid, (tokenization, feature_repr) in test_cases.items():
         print(f"Processing {tid}")
         run_state_of_art(tokenization, feature_repr, tid)
-
-
-
--- proj/test_cases.py
+++ proj/test_cases.py
@@ -3,45 +3,60 @@ import os
 from main import *
 from sklearn.model_selection import StratifiedKFold
 
-def run_experiment(X, y, tokenization, feature_repr, strategy, budget):
-    if tokenization == "lemmatization+stopword":
-        tokenizer = spacy_lemmatizer
-    elif tokenization == "whitespace":
-        tokenizer = simple_tokenizer
-    else:
-        raise ValueError(f"Unknown tokenization: {tokenization}")
-    X_vect, vectorizer = extract_features(X, tokenizer, vectorizer_type=feature_repr)
+
+def run_experiment(X_vect, y, strategy, budget):
     preds, queried_mask = simulate_online_learning(X_vect, y, strategy=strategy, budget=budget)
     metrics = evaluate(preds, y)
+    metrics['predictions'] = preds
+    metrics['true_labels'] = y
     return metrics
+
 
 if __name__ == "__main__":
     test_cases = {
-    "T1": ("lemmatization+stopword", "tfidf", "margin", 0.10),
-    "T2": ("lemmatization+stopword", "tfidf", "margin", 0.20),
-    "T3": ("lemmatization+stopword", "tfidf", "margin", 0.30),
-    "T4": ("whitespace", "tf", "margin", 0.10),
-    "T5": ("whitespace", "tf", "margin", 0.20),
-    "T6": ("whitespace", "tf", "margin", 0.30),
-    "T7": ("lemmatization+stopword", "tfidf", "random", 0.10),
-    "T8": ("lemmatization+stopword", "tfidf", "random", 0.20),
-    "T9": ("lemmatization+stopword", "tfidf", "random", 0.30),
-    "T10": ("lemmatization+stopword", "tfidf", "oracle", 1.00),
-}
+        # "T1": ("TK1", "margin", 0.10),
+        # "T2": ("TK1", "margin", 0.20),
+        # "T3": ("lemmatization+stopword", "tfidf", "margin", 0.30),
+        # "T4": ("TK2", "margin", 0.10),
+        # "T5": ("TK2", "margin", 0.20),
+        # "T6": ("TK2", "margin", 0.30),
+        # "T7": ("TK1", "random", 0.10),
+        "T8": ("TK1", "random", 0.20),
+        "T9": ("TK1", "random", 0.30),
+        "T10": ("TK1", "oracle", 1.00),
+    }
+
+    token_test_cases = {
+        "TK1": ("lemmatization+stopword", "tfidf"),
+        "TK2": ("whitespace", "tf"),
+    }
 
     os.makedirs("test_results", exist_ok=True)
 
     csv_path = "data/enron_dataset.csv"
-    df = load_enron_csv_dataset(csv_path, max_rows=1000)
+    df = load_enron_csv_dataset(csv_path)
     X = df['text']
     y = df['label']
 
+    X_vectorized_dict = {}
+
+    for tkid, (tokenization, feature_repr) in token_test_cases.items():
+        if tokenization == "lemmatization+stopword":
+            tokenizer = spacy_lemmatizer
+        elif tokenization == "whitespace":
+            tokenizer = simple_tokenizer
+        else:
+            raise ValueError(f"Unknown tokenization: {tokenization}")
+        X_vect_temp, _ = extract_features(X, tokenizer, vectorizer_type=feature_repr)
+        X_vectorized_dict[tkid] = X_vect_temp
+
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-    
+
     fold_metrics = []
-    for tid, (tokenization, feature_repr, strategy, budget) in test_cases.items():
+    for tid, (token_case, strategy, budget) in test_cases.items():
         fold_no = 0
         os.makedirs(f"test_results/{tid}", exist_ok=True)
+        X = X_vectorized_dict[token_case]
         for train_index, test_index in skf.split(X, y):
             print(f"processing {tid}, fold {fold_no}")
             X_train = X.iloc[train_index].reset_index(drop=True)
@@ -51,10 +66,8 @@ if __name__ == "__main__":
             fold_no += 1
 
             metrics = run_experiment(
-                X=X_train,
+                X_vect=X_train,
                 y=y_train,
-                tokenization=tokenization,
-                feature_repr=feature_repr,
                 strategy=strategy,
                 budget=budget
             )
@@ -63,5 +76,5 @@ if __name__ == "__main__":
             save_path = os.path.join("test_results", f"{tid}", f"{tid}_fold_{fold_no}.npy")
             np.save(save_path, metrics)
 
-accuracies = [m['accuracy'] for m in fold_metrics]
-print(f"Cross-validated accuracy: {np.mean(accuracies):.4f}")
+    accuracies = [m['accuracy'] for m in fold_metrics]
+    print(f"Cross-validated accuracy: {np.mean(accuracies):.4f}")
